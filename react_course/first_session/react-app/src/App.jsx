5f7dc2528031440dd774651fa484792c39ac4374
Arch_Alejandro <carrilloalejandro0215@gmail.com>
2026-02-24 13:07:51 -0500

    React Course

---
+++ react_course/first_session/react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import Student from "./Students.jsx";
+
+
+function App() {
+  return(
+    <>
+    <Student name="Spongebob" age={30} isStudent = {false}/>
+    <Student name="Patrick" age={40} isStudent = {true}/>
+    <Student name="Calamardo" age={67} isStudent = {false}/>
+    <Student name="Calamardo" age={67} isStudent = {false}/>
+    <Student />
+    </>
+  );
+}
+    
+
+export default App
