90af7cc1f1807456b9e6ff915bb66a8cf63b6198
Alejandro Carrillo <carrilloalejandro0215@gmail.com>
2026-02-23 19:28:53 -0500

    Initial commit

---
+++ answersGame/src/App.jsx
@@ -0,0 +1,54 @@
+import {useRef,useState} from 'react'
+import './App.css'
+
+
+function App() {
+  const inputRef = useRef();
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(){
+    const task = inputRef.current.value
+    if(!task){
+      return
+    }
+    const newTodo = {
+      text : task,
+      status : "Not Started"
+    };
+    setTodos([...todos,newTodo])
+    inputRef.current.value = ""
+  }
+
+  function TodoItem({todo,index}){
+    return (
+            <li>
+            {todo.text}
+            <select value={todo.status}
+            onChange={(e) => {
+              const newTodos = [...todos];
+              newTodos[index].status = e.target.value;
+              setTodos(newTodos)
+            }}>
+              <option value="Not Started">Not Started</option>
+              <option value="In Progress">In Progress</option>
+              <option value="Completed">Completed</option>
+            </select>
+            </li>
+    )
+  }
+
+
+  return (
+    <div>
+        <h1>Todo list</h1>
+        <input type="text" ref={inputRef}/>
+        <button onClick={handleSubmit}> Submit </button>
+        <ul>
+          {todos.map((todo,index) => {
+            return <TodoItem key={index} todo={todo} index={index}></TodoItem>
+          })}
+        </ul>
+    </div>
+  )
+}
+export default App
