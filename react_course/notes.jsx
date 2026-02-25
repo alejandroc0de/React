c2f9ef89e86503ac536b3d86afaa108a1043bf6d
Arch_Alejandro <carrilloalejandro0215@gmail.com>
2026-02-24 21:55:44 -0500

    last part of tutorial for today 1.6 hours left

--- react_course/notes.jsx
+++ react_course/notes.jsx
@@ -311,3 +311,59 @@ function Component(){
         </>
     )
 }
+
+////////////////////////////////////////////////////////////////////////////////////
+// PROGRAMA TODO ALIKE! CARS CHOOSER !!!
+
+
+function Component(){
+
+    const[cars,setCars] = useState([])
+    const[carYear, setCarYear] = useState(new Date().getFullYear())
+    const[carMake, setCarMake] = useState("")
+    const[carModel, setCarModel] = useState("")
+
+    function handleAddCar(){
+        const newCar = {year: carYear, model: carModel, make: carMake} // current state since we are using on change on inputs, so its already saved
+        setCars(c => [...c, newCar])
+
+        setCarYear(new Date().getFullYear())
+        setCarMake("")
+        setCarModel("")
+    }
+
+    function handleRemoveCar(index){
+        setCars(c => c.filter((car,posicion) => posicion !== index))
+    }
+
+    function handleYearChange(event){
+        setCarYear(event.target.value)
+
+    }
+
+    function handleMakeChange(event){
+        setCarMake(event.target.value)
+
+    }
+
+    function handleModelChange(event){
+        setCarModel(event.target.value)
+
+    }
+
+
+    return(
+        <div>
+            <h2>List of car </h2>
+            <ul>
+                {cars.map((car,index) => <li onClick={()=> handleRemoveCar(index)} key={index}>{car.year} {car.make} {car.model} </li>)}
+
+            </ul>
+            <input type="number" value={carYear} onChange={handleYearChange} placeholder="Year" /> <br />
+            <input type="text" value={carMake} onChange={handleMakeChange} placeholder="Make"/> <br />
+            <input type="text" value={carModel} onChange={handleModelChange} placeholder="Model" /> <br />
+
+            <button onClick={handleAddCar}>Add Car</button>
+        </div>
+    )
+}
